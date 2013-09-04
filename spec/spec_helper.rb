2fb604eaed3d8250d8a8135e902c220faa88e4e4
pvdvreede <paul@vdvreede.net>
2013-09-04 16:41:52 +1000

    Explicitly set the Simplecov root for travis-ci.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@
 
 require 'simplecov'
 SimpleCov.minimum_coverage 90
-SimpleCov.start
+SimpleCov.start do
+  root File.expand_path('../../', __FILE__)
+  add_filter '/spec/'
+end
 
 require 'parslet/rig/rspec'
 require 'edifact_parser'
